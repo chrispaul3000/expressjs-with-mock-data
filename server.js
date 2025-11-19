191a02530ef6cdc872e8f59c05c43149debf306e
Christian Paul <christianpauljt@gmail.com>
2025-11-20 06:51:03 +0800

    creating module

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Express is working");
+});
+
+app.listen(PORT, () => {
+    console.log('Server is running at PORT 3000');
+});
